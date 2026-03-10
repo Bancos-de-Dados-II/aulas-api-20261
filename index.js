613288407139e951f267cc7280fa36454f8c4149
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-03-10 14:36:42 -0300

    Realizando a conexão com o banco de dados postgis com o sequelize

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.PG_DATABASE,
+    process.env.PG_USER, process.env.PG_PASSWORD, {
+  host: process.env.PG_HOST,
+  dialect: 'postgres'
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
