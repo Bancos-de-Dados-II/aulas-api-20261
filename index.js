410587d439527644cc35c8af5cf6d4e6d514b8ad
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-03-11 16:00:46 -0300

    Criando método para salvar e listar usuários

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 import Usuario from "./model/usuario.js";
+
+//CREATE
+await Usuario.create({
+    email:"joao@gmail.com",
+    nome:"João"})
+    .then(usuario=> 
+            console.log("Usuário criado "+usuario))
+    .catch(error => console.log("Erro ao criar"+error));
+
+//READ
+//console.log(await Usuario.findAll());
