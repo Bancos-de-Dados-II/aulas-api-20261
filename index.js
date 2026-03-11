c08ac413ffe2df23706f5e8e5ffb588a0853f2b9
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-03-11 16:30:25 -0300

    Criando métodos para atualizar e deletar

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import Usuario from "./model/usuario.js";
 
 //CREATE
-await Usuario.create({
-    email:"joao@gmail.com",
-    nome:"João"})
-    .then(usuario=> 
-            console.log("Usuário criado "+usuario))
-    .catch(error => console.log("Erro ao criar"+error));
+//await Usuario.create({
+//    email:"maria@gmail.com",
+//    nome:"Maria"})
+//    .then(usuario=> 
+//            console.log("Usuário criado "+usuario))
+//    .catch(error => console.log("Erro ao criar"+error));
 
 //READ
 //console.log(await Usuario.findAll());
+
+//UPDATE
+//const usuario = await Usuario.findByPk('joao@gmail.com');
+//usuario.set({nome:"João da Silva"});
+//await usuario.save();
+
+//DELETE
+const usuario = await Usuario.findByPk('joao@gmail.com');
+//usuario.destroy();
+console.log(usuario);
