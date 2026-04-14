17563f777b27a478767d2e7f62f7e0819d034944
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-04-14 14:05:38 -0300

    atualizando modelo de usuário

--- model/usuario.js
+++ model/usuario.js
@@ -2,16 +2,31 @@ import sequelize from '../database/sequelize.js'
 import { DataTypes } from 'sequelize';
 
 const Usuario = sequelize.define('Usuario',{
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
     email:{
         type: DataTypes.STRING,
-        primaryKey: true
+        unique: true,
+        allowNull: false
     },
     nome:{
         type: DataTypes.STRING,
         allowNull: false
+    },
+    localizacao:{
+        type: DataTypes.GEOMETRY('POINT', 4326),
+        allowNull: false
+    },
+    ativo:{
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false
     }
 });
 
-await Usuario.sync();
+await Usuario.sync({force: true});
 
 export default Usuario;
