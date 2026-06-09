d8dcc315db01c3fb7e18eb7751c7305ee23315c1
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-09 14:40:22 -0300

    removendo o sequelize

--- database/mongoose.js
+++ database/mongoose.js
@@ -3,8 +3,6 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 
-await mongoose.connect(process.env.MONGO_URL, ()=>{
-    console.log('Conectado ao MongoDB');
-});
+await mongoose.connect(process.env.MONGO_URL);
 
 export default mongoose;
