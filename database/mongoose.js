8b1163cc88229580e91bf4ce084b142121cf0f13
Paulo Freitas <paulo.freitas.nt@gmail.com>
2026-06-09 14:49:49 -0300

    Adicionando mensagem na conexão com o mongo

--- database/mongoose.js
+++ database/mongoose.js
@@ -4,5 +4,10 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 await mongoose.connect(process.env.MONGO_URL);
+console.log('Conectado ao MongoDB');
+
+mongoose.connection.on('error', (err) => {
+    console.error('Erro de conexão com MongoDB:', err);
+});
 
 export default mongoose;
